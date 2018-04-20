116610df05f97df14a795582d6c309214a82f875
Oleksandr Yefremov <oleksandr.yefremov@prosiebensat1digital.de>
2018-04-20 21:43:22 +0200

    RepoListAdapter

--- app/src/main/java/com/test/scalablecapitaltest/common/Result.kt
+++ app/src/main/java/com/test/scalablecapitaltest/common/Result.kt
@@ -1,11 +1,5 @@
 package com.test.scalablecapitaltest.common
 
-//sealed class Result<out T : Any>
-//
-//data class Success<out T : Any>(val data: T) : Result<T>()
-//
-//data class Failure(val error: Throwable?) : Result<Nothing>()
-
 interface Result<in T> {
     fun onSuccess(data: T)
     fun onFailure(error: Throwable)
